8766a480da56a2d47a5783858e177530a03fdcf7
kinajin <kinajin22@gmail.com>
2023-11-27 21:03:44 +0900

    feature: update location images

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const locationContainer = document.querySelector(".locationContainer");
 locations.forEach((location) => {
   const locationBox = document.createElement("div");
   const countryText = document.createElement("div");
-  const regionText = document.createElement("div"); // 추가됨
+  const regionText = document.createElement("div");
+
   locationBox.classList.add("locationBox");
   countryText.classList.add("countryText");
-  regionText.classList.add("regionText"); // 추가됨
-  locationBox.style.backgroundImage = `url(${location.url})`;
+  regionText.classList.add("regionText");
+
+  locationBox.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${location.image})`;
   countryText.innerText = `${location.country}`;
-  regionText.innerText = `${location.region}`; // 추가됨
+  regionText.innerText = `${location.region}`;
+
   locationBox.appendChild(countryText);
-  locationBox.appendChild(regionText); // 추가됨
+  locationBox.appendChild(regionText);
   locationContainer.appendChild(locationBox);
+
+  locationBox.addEventListener("click", () => {
+    window.location.href = location.link;
+  });
 });
